d455bb0dcfe1c49e448a31f54cebcb69b300ac8b
firesoflife <bryangoertz@gmail.com>
2021-04-09 14:23:38 -0400

    general layout and menu

---
+++ db/migrate/20210325140123_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration[6.1]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.date :acivity_date
+      t.decimal :duration
+
+      t.timestamps
+    end
+  end
+end
